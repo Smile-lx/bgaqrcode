a086ac09b17e4f36d8341ee019dd2f4f0ac19200
Smile-lx <17695520280@163.com>
2019-11-22 15:03:13 +0800

    zbar扫描二维码

--- app/src/main/java/com/example/camera/MainActivity.kt
+++ app/src/main/java/com/example/camera/MainActivity.kt
@@ -3,103 +3,55 @@ package com.example.camera
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.os.Vibrator
 import android.util.Log
-import android.view.View
-import android.widget.Toast
-
+import androidx.appcompat.app.AppCompatActivity
 import cn.bingoogolapple.qrcode.core.QRCodeView
-import cn.bingoogolapple.qrcode.zxing.ZXingView
-import kotlinx.android.synthetic.main.activity_main.*
+import cn.bingoogolapple.qrcode.zbar.ZBarView
 
 
-class MainActivity : Activity() {
-    private var mQRCodeView: QRCodeView? = null
-    private var activity: Activity? = null
+class MainActivity : AppCompatActivity(), QRCodeView.Delegate {
+    private var mZBarView: ZBarView? = null
 
+    override fun onScanQRCodeSuccess(result: String?) {
+        Log.i(TAG, "result:$result")
+        title = "扫描结果为：$result"
+        vibrate()
+        mZBarView!!.startSpot() // 开始识别
+    }
+
+    override fun onScanQRCodeOpenCameraError() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
     override fun onCreate(state: Bundle?) {
         super.onCreate(state)
-        //单纯测试git
         setContentView(R.layout.activity_main)
-        activity = this
-        val zxingview = zxingview
-        mQRCodeView = zxingview as ZXingView
-        mQRCodeView!!.changeToScanQRCodeStyle() //扫二维码
-        mQRCodeView!!.startSpot()  //开启全局一直扫描
-        mQRCodeView!!.setDelegate(object : QRCodeView.Delegate {
+//        setSupportActionBar(findViewById(R.id.toolbar) as Toolbar)
 
-            override fun onScanQRCodeSuccess(result: String) {
-                Log.d("二维码扫描结果", "result:$result")
-
-                Toast.makeText(activity, result, Toast.LENGTH_LONG).show()
-
-                //扫描得到结果震动一下表示
-                vibrate()
-
-                //获取结果后三秒后，重新开始扫描
-                Handler().postDelayed(Runnable { mQRCodeView!!.startSpot() }, 1000)
-            }
-
-            override fun onScanQRCodeOpenCameraError() {
-                Toast.makeText(activity, "打开相机错误！", Toast.LENGTH_SHORT).show()
-            }
-        })
-        val startSpot = start_spot
-        startSpot.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(view: View) {
-                mQRCodeView!!.startSpot()
-                Toast.makeText(activity, "startSpot", Toast.LENGTH_SHORT).show()
-            }
-        })
-        val stopSpot = stop_spot
-        stopSpot.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(view: View) {
-                mQRCodeView!!.stopSpot()
-                Toast.makeText(activity, "stopSpot", Toast.LENGTH_SHORT).show()
-            }
-        })
-        val openFlashlight = open_flashlight
-        openFlashlight.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(view: View) {
-                mQRCodeView!!.openFlashlight()
-                Toast.makeText(activity, "openFlashlight", Toast.LENGTH_SHORT).show()
-            }
-        })
-        val closeFlashlight = close_flashlight
-        closeFlashlight.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(view: View) {
-                mQRCodeView!!.closeFlashlight()
-                Toast.makeText(activity, "closeFlashlight", Toast.LENGTH_SHORT).show()
-            }
-        })
-
-        //        findViewById(R.id.scan_qrcode).setOnClickListener(new View.OnClickListener(){
-        //            @Override
-        //            public void onClick(View view) {
-        //                mQRCodeView.changeToScanQRCodeStyle();
-        //                Toast.makeText(activity,"changeToScanQRCodeStyle",Toast.LENGTH_SHORT).show();
-        //            }
-        //        });
+        mZBarView = findViewById(R.id.zbarview) as ZBarView
+        mZBarView!!.setDelegate(this)
     }
 
     override fun onStart() {
         super.onStart()
-        mQRCodeView!!.startCamera()
+        mZBarView!!.startCamera()
         //强制手机摄像头镜头朝向前边
         //mQRCodeView.startCamera(Camera.CameraInfo.CAMERA_FACING_FRONT);
 
-        mQRCodeView!!.showScanRect() //显示扫描方框
+//        mQRCodeView!!.showScanRect() //显示扫描方框
+        mZBarView!!.startSpotAndShowRect() // 显示扫描框，并开始识别
+
     }
 
     override fun onStop() {
-        mQRCodeView!!.stopCamera()
+        mZBarView!!.stopCamera()
         super.onStop()
     }
 
     override fun onDestroy() {
-        mQRCodeView!!.onDestroy()
+        mZBarView!!.onDestroy()
         super.onDestroy()
     }
 
@@ -108,4 +60,38 @@ class MainActivity : Activity() {
         val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         vibrator.vibrate(200)
     }
+
+    companion object {
+        private val TAG = MainActivity::class.java!!.getSimpleName()
+        private val REQUEST_CODE_CHOOSE_QRCODE_FROM_GALLERY = 666
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        mZBarView!!.showScanRect()
+
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_CHOOSE_QRCODE_FROM_GALLERY) {
+//            val picturePath = BGAPhotoPickerActivity.getSelectedPhotos(data).get(0)
+//            mZBarView.decodeQRCode(picturePath)
+
+        }
+    }
+    override fun onCameraAmbientBrightnessChanged(isDark: Boolean) {
+        // 这里是通过修改提示文案来展示环境是否过暗的状态，接入方也可以根据 isDark 的值来实现其他交互效果
+        var tipText = mZBarView!!.getScanBoxView().tipText
+        val ambientBrightnessTip = "\n环境过暗，请打开闪光灯"
+        if (isDark) {
+            if (!tipText.contains(ambientBrightnessTip)) {
+                mZBarView!!.getScanBoxView().tipText = tipText + ambientBrightnessTip
+            }
+        } else {
+            if (tipText.contains(ambientBrightnessTip)) {
+                tipText = tipText.substring(0, tipText.indexOf(ambientBrightnessTip))
+                mZBarView!!.getScanBoxView().tipText = tipText
+            }
+        }
+    }
+
+
 }
